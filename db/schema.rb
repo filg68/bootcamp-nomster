130fe4751cbd0d10fe7817ba1eccf76b73ed05e8
Fil <runfilrun@gmail.com>
2019-03-06 02:15:31 +0000

    Re ran migration of data as field names where not showing up.  Edited #####_create_places.rb file to include missing field names and that seemed to fix it

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 2019_03_05_215203) do
   enable_extension "plpgsql"
 
   create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
