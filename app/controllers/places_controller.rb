0daa0c5259a04e5cf110c0f6e39b59474d608ac5
Fil <runfilrun@gmail.com>
2019-03-09 15:47:17 +0000

    Created functionality to delete restaurant data by adding a destroy button to the show place.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -31,6 +31,12 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
+
   def place_params
     # strips the values from the form and makes them available for use
     params.require(:place).permit(:name, :description, :address)
